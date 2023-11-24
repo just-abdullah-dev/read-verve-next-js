750bfb4fe25a327e1b62973f4c17736dc3f5b288
Abdullah <126217821+just-abdullah-dev@users.noreply.github.com>
2023-11-24 21:39:48 +0500

    Update stables.js

--- constants/stables.js
+++ constants/stables.js
@@ -1,5 +1,5 @@
 const UPLOAD_FOLDER_BASE_URL = 'http://localhost:3000/uploads/';
 // const BASE_URL = 'http://localhost:3000';
-const BASE_URL = "https://read-verve-next-dve37ahu7-just-abdullah-dev.vercel.app";
+const BASE_URL = "https://read-verve-next-9hstcf229-just-abdullah-dev.vercel.app";
 const stables = {UPLOAD_FOLDER_BASE_URL, BASE_URL};
 export default stables;
